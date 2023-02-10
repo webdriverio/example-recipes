37d466a0e844afdfe450299661a47a073c9d2979
Christian Bromann <git@bromann.dev>
2023-02-09 23:53:37 -0800

    skip timezone test

--- timezone/custom-timezones.js
+++ timezone/custom-timezones.js
@@ -13,12 +13,7 @@ const browser = await remote({
 const pptr = await browser.getPuppeteer()
 const page = (await pptr.pages())[0]
 await page.emulateTimezone('Africa/Douala')
-
 await browser.url('https://webbrowsertools.com/timezone/')
-await browser.waitUntil(async () => (
-    (await browser.getTitle()) !== 'Just a moment...'),
-    { timeout: 10 * 1000 }
-)
 
 assert.equal(
     await browser.$('#timeZone').getText(),
