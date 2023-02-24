355434bdef13d29608d6d5fbfbeaa034c8a2aa74
Christian Bromann <git@bromann.dev>
2023-02-23 23:39:15 -0800

    add comments

--- keys/keys.js
+++ keys/keys.js
@@ -4,9 +4,12 @@ it('should copy&paste from one input to another', async () => {
     const $copyInput = await $('#copy')
     const $pasteInput = await $('#paste')
 
+    // copies text from an input element
     await $copyInput.setValue('some text')
     await browser.keys([Key.Ctrl, 'a'])
     await browser.keys([Key.Ctrl, 'c'])
+
+    // inserts text from clipboard into input element
     await $pasteInput.click()
     await browser.keys([Key.Ctrl, 'v'])
 
