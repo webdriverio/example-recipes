de5c3710da60c8454f2225950921e8792274b85c
Christian Bromann <git@bromann.dev>
2023-02-23 23:30:55 -0800

    reformat code for better presentation

--- keys/keys.js
+++ keys/keys.js
@@ -1,20 +1,18 @@
 import { Key } from 'webdriverio'
 
-describe('waitForDisplayed Example', () => {
-    beforeEach(async () => {
-        await browser.url('/index.html')
-    })
+it('should copy&paste from one input to another', async () => {
+    const $copyInput = await $('aria/copy')
+    const $pasteInput = await $('aria/paste')
 
-    it('should copy&paste from one input to another', async () => {
-        const $copyInput = await $('aria/copy')
-        const $pasteInput = await $('aria/paste')
+    await $copyInput.setValue('some text')
+    await browser.keys([Key.Ctrl, 'a'])
+    await browser.keys([Key.Ctrl, 'c'])
+    await $pasteInput.click()
+    await browser.keys([Key.Ctrl, 'v'])
 
-        await $copyInput.setValue('some text')
-        await browser.keys([Key.Ctrl, 'a'])
-        await browser.keys([Key.Ctrl, 'c'])
-        await $pasteInput.click()
-        await browser.keys([Key.Ctrl, 'v'])
+    await expect($pasteInput).toHaveValue('some text')
+})
 
-        await expect($pasteInput).toHaveValue('some text')
-    })
+beforeEach(async () => {
+    await browser.url('/index.html')
 })
