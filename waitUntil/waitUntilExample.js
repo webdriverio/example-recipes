64f4720869b6c8da77cc71fd771d491050b99e66
Sri Harsha <12621691+harsha509@users.noreply.github.com>
2023-05-24 11:50:57 +0530

    add: waitUntil example for browser (#89)

--- waitUntil/waitUntilExample.js
+++ waitUntil/waitUntilExample.js
@@ -1,9 +1,9 @@
-describe('waitForDisplayed Example', () => {
+describe('waitUntil Example', () => {
     beforeEach(async () => {
         await browser.url('/index.html')
     })
 
-    it('should wait until text has changed', async () => {
+    it('element should wait until text has changed', async () => {
         const elem = await $('#someText')
         await elem.waitUntil(async function () {
             return (await this.getText()) === 'I am now different'
@@ -11,5 +11,15 @@ describe('waitForDisplayed Example', () => {
             timeout: 5000,
             timeoutMsg: 'expected text to be different after 5s'
         })
+    })
+
+    it('browser should wait until text has changed', async () => {
+      const elem = await $('#someText')
+      await browser.waitUntil(async function () {
+        return (await elem.getText()) === 'I am now different'
+      }, {
+        timeout: 5000,
+        timeoutMsg: 'expected text to be different after 5s'
+      })
     })
 })
