cf1221b1436d1aec6df94e274124178ae7514c3d
Christian Bromann <git@bromann.dev>
2023-06-14 10:32:43 +0200

    apply on devtools

--- setup/devtools.js
+++ setup/devtools.js
@@ -13,12 +13,14 @@ const client = await DevTools.newSession({
 await client.navigateTo('https://www.google.com/ncr')
 
 const approveCookieBtns = await client.findElements('css selector', 'button div[role="none"]')
-await client.elementClick(approveCookieBtns[approveCookieBtns.length - 1]['element-6066-11e4-a52e-4f735466cecf'])
+if (approveCookieBtns.length) {
+    await client.elementClick(approveCookieBtns[approveCookieBtns.length - 1]['element-6066-11e4-a52e-4f735466cecf'])
+}
 
 const searchInput = await client.findElement('css selector', 'textarea')
-await client.elementSendKeys(searchInput['element-6066-11e4-a52e-4f735466cecf'], 'DevTools')
+await client.elementSendKeys(searchInput['element-6066-11e4-a52e-4f735466cecf'], 'WebDriver')
 const submitBtns = await client.findElements('css selector', 'input[value="Google Search"]')
-await client.elementClick(submitBtns[1]['element-6066-11e4-a52e-4f735466cecf'])
+await client.elementClick(submitBtns[0]['element-6066-11e4-a52e-4f735466cecf'])
 
 const title = await client.getTitle()
 console.log(title) // outputs "DevTools - Google Search"
