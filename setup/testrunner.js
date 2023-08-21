14ae61a54f9c37f8da358d1c70f61aa3a37e8d12
Christian Bromann <git@bromann.dev>
2023-08-21 11:09:02 -0700

    fix duckduckgo test

--- setup/testrunner.js
+++ setup/testrunner.js
@@ -4,8 +4,8 @@ describe('DuckDuckGo search', () => {
     it('Searches for WebdriverIO', async () => {
         await browser.url('https://duckduckgo.com/')
 
-        await browser.$('#search_form_input_homepage').setValue('WebdriverIO')
-        await browser.$('#search_button_homepage').click()
+        await browser.$('aria/Search with DuckDuckGo').setValue('WebdriverIO')
+        await browser.$('aria/Search').click()
 
         const title = await browser.getTitle()
         expect(title).toBe('WebdriverIO at DuckDuckGo')
