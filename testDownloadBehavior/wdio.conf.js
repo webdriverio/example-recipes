84dda93011234d0b2a34ee0cfb3cdfa2a06136a5
Christian Bromann <git@bromann.dev>
2024-02-06 15:14:26 -0800

    minor formatting

--- testDownloadBehavior/wdio.conf.js
+++ testDownloadBehavior/wdio.conf.js
@@ -4,48 +4,56 @@ import { config as baseConfig } from '../wdio.conf.js'
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
-const chromeCaps = {
-    browserName: 'chrome',
-    "goog:chromeOptions": {
-        args: ['headless', 'disable-gpu'],
-        prefs: {
-            "download.default_directory": __dirname
+const chromeOptions = {
+    capabilities: {
+        browserName: 'chrome',
+        "goog:chromeOptions": {
+            args: ['headless', 'disable-gpu'],
+            prefs: {
+                "download.default_directory": __dirname
+            }
         }
     }
 }
 
-const firefoxCaps = {
-    browserName: 'firefox',
-    "moz:debuggerAddress": true,
-    "moz:firefoxOptions": {
-        args: ['-headless'],
-        prefs: {
-            "browser.download.dir": __dirname,
-            "browser.download.folderList": 2,
-            "browser.download.manager.showWhenStarting": false,
-            "browser.helperApps.neverAsk.saveToDisk": "*/*"
+const firefoxOptions = {
+    capabilities: {
+        browserName: 'firefox',
+        "moz:debuggerAddress": true,
+        "moz:firefoxOptions": {
+            args: ['-headless'],
+            prefs: {
+                "browser.download.dir": __dirname,
+                "browser.download.folderList": 2,
+                "browser.download.manager.showWhenStarting": false,
+                "browser.helperApps.neverAsk.saveToDisk": "*/*"
+            }
         }
     }
 }
 
-const edgeCaps = {
-    browserName: 'edge',
-    "ms:edgeOptions": {
-        args: ['--headless'],
-        prefs: {
-            "download.default_directory": __dirname
+const edgeOptions = {
+    capabilities: {
+        browserName: 'edge',
+        "ms:edgeOptions": {
+            args: ['--headless'],
+            prefs: {
+                "download.default_directory": __dirname
+            }
         }
     }
 }
 
 const browserCapabilities = {
-    chrome: chromeCaps,
-    firefox: firefoxCaps,
-    edge: edgeCaps
+    chrome: chromeOptions.capabilities,
+    firefox: firefoxOptions.capabilities,
+    edge: edgeOptions.capabilities
 }
-const capabilities = process.env.BROWSER
-    ? browserCapabilities[process.env.BROWSER]
-    : chromeCaps
+const capabilities = [
+    process.env.BROWSER && browserCapabilities[process.env.BROWSER]
+        ? browserCapabilities[process.env.BROWSER]
+        : chromeCaps
+]
 
 export const config = {
     ...baseConfig,
